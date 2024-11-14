4a04cefd628bbc5faa95e90cc5eecdaa08d5ecb8
Cakes0nStove <126332673+Cakes0nStove@users.noreply.github.com>
2024-11-14 16:09:35 +0000

    Circular.java

--- Circular.java
+++ Circular.java
@@ -1,30 +1,37 @@
 
 public class Circular {
 	public static void main(String[] args) {
-//	    long startTime = System.currentTimeMillis();
-//		int num = 500000;
-//		double bestNum = 10000;
-//		int tempN = 3;
-//		int tempD = 1;
-//		
-//		System.out.println(1/3);
-//		
-//		for(int i = 1; i < num / 3.3; i++) {
-//			for(int j = (int)( 3.2 * i); j < 4 * i ; j++) {
-//				var diff = Math.abs(((double)j/i) - Math.PI);
-//				if(diff < bestNum) 
-//				{
-//					bestNum = diff;
-//					tempN = j;
-//					tempD = i;
-//				
-//				}
-//			}
-//		}
-//		System.out.print(tempN + "/" + tempD +"    error = " + bestNum);
-//		
-//		long endTime = System.currentTimeMillis();
-//		System.out.println("          time =" + ((endTime-startTime)/1000)+"s");
+		//Calculate the closest rational number to PI,
+		// the idea behind this is to use the principle of bruteforcing and range reducing to speed up the algorithm.
+	    long startTime = System.currentTimeMillis();
+		int num = 500000;
+		double bestNum = 10000;
+		int tempN = 3;
+		int tempD = 1;
+		
+		System.out.println(1/3);
+		
+		for(int i = 1; i < num / 3.3; i++) {
+			for(int j = (int)( 3.2 * i); j < 4 * i ; j++) {
+				var diff = Math.abs(((double)j/i) - Math.PI);
+				if(diff < bestNum) 
+				{
+					bestNum = diff;
+					tempN = j;
+					tempD = i;
+				
+				}
+			}
+		}
+		System.out.print(tempN + "/" + tempD +"    error = " + bestNum);
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println("          time =" + ((endTime-startTime)/1000)+"s");
+
+
+		//This code underneath is used to create a custom timer when you input the seconds
+		// the program then calculates how many hours, mins,sec, and millisec there are 
+		//within those seconds
 	
 		
 		double time = 3661.001;//seconds
