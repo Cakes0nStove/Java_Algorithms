9642e8eff0240a7e0be9bbc0e6da0d0cfdce7017
Cakes0nStove <126332673+Cakes0nStove@users.noreply.github.com>
2024-11-14 16:07:28 +0000

    Add files via upload

---
+++ Circular.java
@@ -0,0 +1,57 @@
+
+public class Circular {
+	public static void main(String[] args) {
+//	    long startTime = System.currentTimeMillis();
+//		int num = 500000;
+//		double bestNum = 10000;
+//		int tempN = 3;
+//		int tempD = 1;
+//		
+//		System.out.println(1/3);
+//		
+//		for(int i = 1; i < num / 3.3; i++) {
+//			for(int j = (int)( 3.2 * i); j < 4 * i ; j++) {
+//				var diff = Math.abs(((double)j/i) - Math.PI);
+//				if(diff < bestNum) 
+//				{
+//					bestNum = diff;
+//					tempN = j;
+//					tempD = i;
+//				
+//				}
+//			}
+//		}
+//		System.out.print(tempN + "/" + tempD +"    error = " + bestNum);
+//		
+//		long endTime = System.currentTimeMillis();
+//		System.out.println("          time =" + ((endTime-startTime)/1000)+"s");
+	
+		
+		double time = 3661.001;//seconds
+		double hours2= (double) (time / 3600);//1.0169
+		double hours = (int)(time / 3600);//1
+		
+		double mins2 = (hours2 - hours) * 60;//1.0169 -1 % 60 = 1.0166
+		double mins = (int)mins2;//1
+		double seconds = (int)((mins2 - mins) * 60);//1.0
+		double seconds2 =(mins2 - mins) * 60;//1.001
+		double milliseconds = (int)((seconds2-seconds)* 1000);
+//HH:MM:SS:mmm		
+		
+		System.out.println("Hours =" + hours);
+		System.out.println("Minutes =" + mins);
+		System.out.println("Seconds =" + seconds);
+		
+		System.out.printf((int)hours+":"+(int)mins+":"+(int)seconds+":"+(int)milliseconds);
+		
+		
+	}
+		
+		
+
+	
+	
+	
+	
+}
+
